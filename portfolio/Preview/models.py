c6744d9f0a304395d918b011cf580af2c8c716dc
Elena.Kokhovets <alena.kokhovets@gmail.com>
2023-02-01 23:52:01 +0300

    Initial Preview 24

--- portfolio/Preview/models.py
+++ portfolio/Preview/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Project(models.Model):
+    title = models.CharField(max_length=30)  #название поста и макс кол-во символов
+    description = models.TextField()
+    tags = models.CharField(max_length=25) #описываем теги проекта огранич по длине
+    image = models.FileField(upload_to='img/') #прописываем где храниться кэш картинок 'img/' папка каталог где храняться картинки
+
 # Create your models here.
--- portfolio/Preview/views.py
+++ portfolio/Preview/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Project
 
-def Preview(request):
-    return render(request, 'Preview.html')
+def project_index(request): #индексное представление всех постов
+    vse_posti = Project.objects.all() #в переменную vse_posti собрали все посты в таблице Project
+    context = {
+        'vse_posti':vse_posti
+    } #закидываем в переменную словарь
+    return render(request, 'project_index.html', context)
+
+def project_detail(request,pk): #id первичный ключ конкретного поста
+    odin_post = Project.objects.get(pk=pk) #в переменную odin_post собрали пост с первичным ключом равным id из таблицы Project
+    context = {
+        'odin_post':odin_post
+    } #закидываем в переменную словарь
+    return render(request, 'project_detail.html', context)
